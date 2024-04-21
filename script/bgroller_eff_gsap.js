3a4e8a79e51bf4af57eefa205c199e1108646370
Gulfy_N0rris <99170315+Gulfy034@users.noreply.github.com>
2024-04-21 12:43:23 +0800

    rm node_modules

---
+++ script/bgroller_eff_gsap.js
@@ -0,0 +1,2 @@
+import _ from '/node_modules/lodash-es/lodash-default'
+import _ from 'lodash-es'
---
+++ script/glitch_eff_gsap.js
@@ -0,0 +1,2 @@
+console.log("auguroz output: ~# i hate js, because it gives me tons of jank errors.")
+console.log("auguroz output: ~# but i do love typescript.")
