b783cd4d32f33862d9184da71b084b730cfb63e5
Gulfy_N0rris <99170315+Gulfy034@users.noreply.github.com>
2024-04-24 00:35:15 +0800

    ignore unstable tsx

--- src/tsx_script/typed_react_eff.tsx
+++ src/tsx_script/typed_react_eff.tsx
@@ -1,11 +1,14 @@
-import { ReactTyped } from "react-typed";
 /* in order to load probably during long-time ------------
+
+import { ReactTyped } from "react-typed";
 
 import axios from 'axios';
 
 const fetcher = () => axios.get(url).then(res => res.data)
 
 add these code ↑↑↑-----------------*/
+
+/* need to be fixed below.
 
 const skyaTityped = () => (
     <div>
@@ -20,3 +23,5 @@ const skyaTityped = () => (
         <br />
     </div>
 )
+
+*/
