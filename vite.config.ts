1e420ee7c0beb29f91d24f5e67936e70fcfb1d59
Gulfy034 <99170315+Gulfy034@users.noreply.github.com>
2025-03-31 13:08:25 +0800

    changed correctly

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import { resolve } from 'node:path';// attention
-// i have changed into deno to replace npm, use node:path to use a npm build-in module!!!
-// you need `npm install --save-dev @types/node` to import `path`,
-// or `install --save-dev path` follishly.
-// why i use deno, this is a mystery.
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
