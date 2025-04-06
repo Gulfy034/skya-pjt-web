6cfa7b89c5bb44268b964182952c77e5e5eb84b9
Gulfy034 <99170315+Gulfy034@users.noreply.github.com>
2025-04-06 23:34:45 +0800

    try i18n

---
+++ components/shared/skya_languageSet.tsx
@@ -0,0 +1,27 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import Backend from "i18next-http-backend";
+
+// TODO: make more translation
+
+
+export default i18n
+    .use(Backend)
+    .use(LanguageDetector)
+    .use(initReactI18next)
+    .init({
+        debug: true,
+        fallbackLng: "en",
+        ns: ["loading", "home"],
+        defaultNS: "loading",
+        backend: {
+            loadPath: "/locales/{{lng}}/{{ns}}.json",
+        },
+        detection: {
+            caches: ["localStorage", "sessionStorage", "cookies"],
+        },
+        interpolation: {
+            escapeValue: false,
+        }
+    })
--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 import {
   createBrowserRouter,
   RouterProvider,
-} from 'react-router-dom';
+} from "react-router-dom";
 
-import IndexPage from './routes/route_Index';
-import DownloadPage from './routes/route_Download';
-import AboutPage from './routes/route_About';
-import ContectPage from './routes/route_Contect';
-import Error404Page from './routes/route_404';
-import { DbgOutputStatus } from "@components/skya_debugging";
+import IndexPage from "./routes/route_Index";
+import DownloadPage from "./routes/route_Download";
+import AboutPage from "./routes/route_About";
+import ContectPage from "./routes/route_Contect";
+import Error404Page from "./routes/route_404";
 
+import "@components/shared/skya_languageSet";
+import { DbgOutputStatus } from "@components/shared/skya_debugging";
 
-//TODO: Make router reflash the page
 
 const root = createBrowserRouter([
   {
