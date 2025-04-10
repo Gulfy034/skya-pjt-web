1421038d3c26a7b116519478f64493a3cfd9554d
Gulfy034 <99170315+Gulfy034@users.noreply.github.com>
2025-04-10 23:56:44 +0800

    removed crt tv

--- components/skya_introduci_02.tsx
+++ components/skya_introduci_02.tsx
@@ -1,75 +1,24 @@
-import { React, useState } from 'react';
-import gsap from 'gsap';
-import CrtSvg from '@components/skya_filters';
-//import { playCrtSfx } from '@components/skya_playsfx';
-//import { imgToAscii } from '@hooks/use-imgToAscii';
-import '@styles/skya_introduci_02.scss';
-import '@styles/skya_crt.scss';
+import { Trans, useTranslation } from "react-i18next";
+//import { playCrtSfx } from "@components/skya_playsfx";
 
-// TODO: Add more buttons, i18n, animations
+import "@styles/skya_introduci_02.scss";
 
-function CrtTV() {
-    return (
-        <div className="crtBox">
-            <input type="checkbox" id="switch" /* checked={playCrtSfx} */ />
-            <label htmlFor="switch" className="switch-label"></label>
-            <button id="crtSwitch"></button>
-            <div className="crtScreen crtScanlines">
-                <div className="crtContents">
-                    <h1 id="crtTitle">
-                        crt title<span id="crtPointer">&#9608;</span>
-                    </h1>
+// TODO: i18n, animations
 
-                    <div id="crtNav">
-                        <ul>
-                            <p>
-                                <li>
-                                    <a id="crtLink" /*onMouseEnter={playsfxTwo} */ href="https://skya-project.website">The web</a>
-                                </li>
-                            </p>
-                            <p>
-                                <li>
-                                    <a id="crtLink" /*onMouseEnter={playsfxTwo} */ href="https://github.com/Gulfy034/skya-pjt-web">GitHub site</a>
-                                </li>
-                            </p>
-                        </ul>
-                    </div>
-
-                    <div id="crtFooter">
-                        <p>I am the crtfooter</p>
-                    </div>
-
-                </div>
-            </div>
-        </div>
-    )
-}
-
-/*
-function CrtBtn() {
-    const [check, isChecked] = useState(false);
-}
-*/
-/*
-function ImgAsciiOne() {
-    let skyademo1 = new imgToAscii("imgs/SKYA_demo_1.png");
-    skyademo1.display();
-    //skyademo1.displaycolor('#2bad1a');
-}
-*/
 export default function IntrTwo() {
+    const {t} = useTranslation(["contents"]);
     return (
         <>
-            <CrtSvg />
             <div id="subTitleTwo">
-                <h1 id="contTitleTwo"></h1>
+                <h3 id="contTitleTwo">
+                    <Trans i18nKey={"contents"}>
+                        {t("title.sectionTitleTwo")}
+                    </Trans>
+                </h3>
             </div>
 
-            
-            <p>the picture of the demo logo</p>
+            <p>pics</p>
             <br />
-
-            <CrtTV />
         </>
     )
 }
