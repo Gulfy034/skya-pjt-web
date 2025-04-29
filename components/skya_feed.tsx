b0628ece7641e2ee9ca57868618399cdb8e4ed90
Gulfy034 <99170315+Gulfy034@users.noreply.github.com>
2025-04-29 22:39:13 +0800

    feed worker test removed

--- components/skya_feed.tsx
+++ components/skya_feed.tsx
@@ -2,22 +2,14 @@
 import { useTranslation } from "react-i18next";
 
 import "@styles/shared/_buttons.scss";
-import { useState } from "react";
 
 // TODO: add rss selection: rss2.0 atom
 
 export default function ButtonFeeder() {
-    const [name, setName] = useState("what");
     const { t } = useTranslation(["nav"]);
     return (
-        <button id="feed" className="navbtnT bi bi-rss" onClick={
-            () => {
-                fetch('/api/')
-                  .then((res) => res.json() as Promise<{ name: string }>)
-                  .then((data) => setName(data.name))
-              }
-        }>
-            {t("feed")+" with "+name}
+        <button id="feed" className="navbtnT bi bi-rss" >
+            {t("feed")}
         </button >
     )
 }
